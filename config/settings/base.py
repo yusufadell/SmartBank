ee821b00393eb0ca6467fc1473bc42497371046e
yusufadell <youseefadel777@gmail.com>
2022-05-31 04:06:04 +0200

    🗃️  Migrate to PostgreSQL database

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,13 +85,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": env.db("DATABASE_URL", default="postgres:///banks"),
 }
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
+
 
 # FIXTURES
 # ------------------------------------------------------------------------------
