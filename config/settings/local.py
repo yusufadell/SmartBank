c70cb7cb54b8d45f23fd97829f127bd758c4737c
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-06-01 22:00:12 +0200

    remvoe whitenoise form inclued apps in local dev

--- config/settings/local.py
+++ config/settings/local.py
@@ -10,15 +10,12 @@ SECRET_KEY = env(
     "SECRET_KEY",
     default="django-insecure-ry00cb&zi1^(5+e9icajm0izcvl--qpp0m0+$q0oke4e#*z&qz",
 )
+
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
-
-# WhiteNoise
-# ------------------------------------------------------------------------------
-# http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
-INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa F405
-
-
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
 # Your stuff...
 # ------------------------------------------------------------------------------
