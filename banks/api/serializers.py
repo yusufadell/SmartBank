06a3b3ba5fa2ad4d9825d8a290c5b1c6fcb74985
Restyled.io <commits@restyled.io>
2022-06-16 21:53:40 +0000

    Restyled by autopep8

--- banks/api/serializers.py
+++ banks/api/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from ..models import Customers, Transfers
 
+
 class TransfersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transfers
         fields = '__all__'
-        
-        
+
+
 class CustomersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customers
