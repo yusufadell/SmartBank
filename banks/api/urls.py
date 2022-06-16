d9c7d5bc1fbcbd5c2fdfbad8eaad49a8aa09c286
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-06-16 23:51:34 +0200

    🍻 map views with url patters

---
+++ banks/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import TransfersListView, TransfersDetailView, CustomersListView, CustomersDetailView
+
+app_name = "banks"
+
+urlpatterns = [
+    path('transfers/', TransfersListView.as_view(), name='transfers-list'),
+    path('transfers/<int:pk>/', TransfersDetailView.as_view(),
+         name='transfers-detail'),
+    path('customers/', CustomersListView.as_view(), name='customers'),
+    path('customers/<int:pk>/', CustomersDetailView.as_view(),
+         name='customers-detail'),
+]
